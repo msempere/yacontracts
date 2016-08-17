524949b0e61d7997de36b0e75233da3dc82c6b6e
MSempere <msempere@gmx.com>
2016-08-17 22:07:07 +0100

    Fixed long description

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 
@@ -9,7 +9,7 @@ description = (
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-long_description = read('README.md')
+long_description = 'YAContracts: Yet Another contracts package'
 
 
 def get_version(filename):
